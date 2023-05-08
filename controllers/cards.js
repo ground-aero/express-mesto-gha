913b936bde888dcf0563430bcd50d2b7d7e32425
Eugene Avdeev <ea.avdeev@gmail.com>
2023-05-08 18:27:43 +0300

    refactor(controllers/cards): getCards,catch()

--- controllers/cards.js
+++ controllers/cards.js
@@ -65,13 +65,11 @@ const createCard = (req, res) => {
  * @param req, /cards, метод GET
  * @param res
  */
-const getCards = (req, res) => Card.find({})
+const getCards = (req, res, next) => Card.find({})
   .populate(['owner', 'likes']) // достанем поле owner, и поле likes
   .then((cards) => res.send({ data: cards })) // res.status(200) добавл по дефолту
-  // .then((cards) => {
-  //   console.log(cards, req.user)
-  // })
-  .catch(() => res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' }));
+  .catch(next);
+  // .catch(() => res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' }));
 
 /** Удаляет карточку
  * @param req, /cards/:cardId — удаляет карточку по идентификатору, метод DELETE
@@ -82,9 +80,9 @@ const getCards = (req, res) => Card.find({})
 const deleteCard = (req, res, next) => {
   const { cardId } = req.params; // extract ID from URL
   Card.findById(cardId)
-    .orFail(() => new NotFoundErr('Нет карточки по заданному ID'))
+    .orFail(() => new NotFoundErr('По заданному ID карточки нет'))
     .then((card) => {
-      if (card.owner.toString() !== req.user._id) {
+      if (card.owner.toString() !== req.user._id) { // req.user._id - это
         return next(new ForbiddenErr('Нельзя удалить чужую карточку!'));
       } else {
         return card.deleteOne()
@@ -93,118 +91,13 @@ const deleteCard = (req, res, next) => {
     })
     .catch((err) => {
       if (err.kind === 'ObjectId') {
-        next(new BadRequestErr('Невалидный ID карточки'));
+        next(new BadRequestErr('Невалидный ID карточки')); // попадет в централиз обработч ошибок
       } else {
         next(err);
       }
-    })
+    });
 
 };
-// const deleteCard = (req, res) => {
-//   const { cardId } = req.params; // extract ID from URL
-//   // const owner = req.owner._id;
-//   const id = req.user._id;
-//   Card.findById(cardId)
-//     // .populate('owner', '_id')
-//     .orFail(() => {
-//       const error = new Error('Карточка по заданному ID отсутствует в Базе');
-//       error.statusCode = 404;
-//       return error;
-//     })
-//     .then((card) => {
-//       console.log(card);
-//       if (card.owner.toString() !== id) {
-//         throw new ForbiddenErr({ message: 'Нет прав на удаление карточки' });
-//       }
-//       Card.findByIdAndDelete(cardId)
-//         .then((cardDeleted) => {
-//           res.send({ data: cardDeleted });
-//         })
-//         .catch(err => {
-//           if (err.name === 'CastError') {
-//             res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные' });
-//           } else {
-//             res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' });
-//           }
-//         })
-//     })
-//     .catch((err) => {
-//       if (err.statusCode === 404) {
-//         res.status(ERR_CODE_404).send({ message: 'Карточка с указанным ID не найдена' });
-//         return;
-//       }
-//       if (err.name === 'CastError') {
-//         res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-//         return;
-//       }
-//       if (err.name === 'ValidationError') {
-//         res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-//         return;
-//       }
-//       res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' });
-//     })
-// };
-  // Card.findByIdAndDelete(cardId)
-  //   .populate(['owner', '_id'])
-  //   .then((card) => {
-  //     res.send({ data: card, message: 'Карточка удалена.....' }); // res.status(200) доб по дефолту
-  //   })
-  //   .catch((err) => {
-  //     if (err.name === 'CastError') {
-  //       res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные' });
-  //     } else {
-  //       res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' });
-  //     }
-  //   });
-  // .catch((err) => {
-  //   if (err.name === 'CastError') {
-  //     res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные' });
-  //   } else {
-  //     res.status(500).send({ message: 'Ошибка по умолчанию' });
-  //   }
-  // })
-// };
-// const deleteCard = (req, res) => {
-//   const { cardId } = req.params;
-//   // const owner = req.owner._id;
-//
-//   Card.findById(cardId)
-//     .populate('owner')
-//     .orFail()
-//     .catch(() => {
-//       throw new NotFoundErr({ message: 'карточка с таким id не найдена,kdl;kj;d ' });
-//       // res.send({ message: 'нет такой карточки' })
-//     })
-//     .then((card) => {
-//       res.send({ data: card, message: 'Карточка найдена.....' }); // res.status(200) доб по дефолту
-//     })
-//     .catch((err) => {
-//       if (err.name === 'CastError') {
-//         res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные' });
-//       } else {
-//         res.status(500).send({ message: 'Ошибка по умолчанию' });
-//       }
-//     });
-//   Card.findByIdAndDelete(cardId)
-//     .populate(['owner', '_id'])
-//     .then((card) => {
-//       res.send({ data: card, message: 'Карточка удалена.....' }); // res.status(200) доб по дефолту
-//     })
-//     .catch((err) => {
-//       if (err.name === 'CastError') {
-//         res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные' });
-//       } else {
-//         res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' });
-//       }
-//     });
-//   // .catch((err) => {
-//   //   if (err.name === 'CastError') {
-//   //     res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные' });
-//   //   } else {
-//   //     res.status(500).send({ message: 'Ошибка по умолчанию' });
-//   //   }
-//   // })
-// };
 // const deleteCard = (req, res) => {
 //   const { cardId } = req.params;
 //   // const owner = req.owner._id;
@@ -233,25 +126,33 @@ const deleteCard = (req, res, next) => {
  * url: "http://localhost:3000/cards/63f61dfbc7eee15ca4adc16e/likes"
  * @param res
  */
-const likeCard = (req, res) => {
+const likeCard = (req, res, next) => {
   const { cardId } = req.params;
   const ownerId = req.user._id;
   // const { _id } = req.user;
   // добавить _id польз-ля в массив лайков, если его в нем нет
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: ownerId } }, { new: true })
     .orFail(new Error('idNotFoundError'))
+    // .orFail(() => new NotFoundErr('такой карточки (ID) не существует'))
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.message === 'idNotFoundError') {
-        res.status(ERR_CODE_404).send({ message: 'Передан несуществующий _id карточки' });
-        return;
-      }
-      if (err.name === 'CastError') {
-        res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные для постановки лайка' });
+      if (err.kind === 'ObjectId') {
+        next(new BadRequestErr('Невалидный ID карточки'));
       } else {
-        res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' });
+        next(err);
       }
     });
+// .catch((err) => {
+//   if (err.message === 'idNotFoundError') {
+//     res.status(ERR_CODE_404).send({ message: 'Передан несуществующий _id карточки' });
+//     return;
+//   }
+//   if (err.name === 'CastError') {
+//     res.status(ERR_CODE_400).send({ message: 'Переданы некорректные данные для постановки лайка' });
+//   } else {
+//     res.status(ERR_CODE_500).send({ message: 'Ошибка по умолчанию' });
+//   }
+// });
 };
 // const likeCard = (req, res) => {
 //   const { cardId } = req.params;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,8 +11,9 @@ const auth = (req, res, next) => {
   // 1.достать 'Bearer' из хедера авторизации, проверять authorization
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthoErr('Необходима авторизация *'); // генерим ошибку,
-    // чтобы затем передать в общий обработчик ошибок
+    throw new AuthoErr('Необходима авторизация *'); // генерим ошибку в синхронном коде,
+    // так допустимо.
+    // Либо через next(new Err..()...) чтобы затем передать в общий обработчик ошибок
     // res.status(401).send({ message: 'Требуется авторизация' });
   }
 
