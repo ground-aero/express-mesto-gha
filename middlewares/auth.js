d4398e01b48e6c5e9f2f05bf798fc053fde725c3
Eugene Avdeev <ea.avdeev@gmail.com>
2023-05-27 10:03:26 +0300

    refactor(middlewares): auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,27 +8,24 @@ const AuthoErr = require('../errors/autho-err');
 // должен проверять хедер определенных запросов на наличие авторизации
 const auth = (req, res, next) => {
 // ToDo: check token valid, and go next. If (valid) {go next}, else error
-  // 1.достать 'Bearer' из хедера авторизации, проверять authorization
+  // взять заголовок authorization
   const { authorization } = req.headers;
-  // 2.брать authorization, достать jwt из authorization хедера,
-  // проверить jwt валидный с помощью библиотеки jsonwebtoken что
+  // если загол. authorization не передан, или не начин. с Bearer
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthoErr('Необходима авторизация *'); // генерим ошибку в синхронном коде
+  }
+
+  //  достать jwt из authorization хедера, удалить Bearer из загол
+  const jwt = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      throw new AuthoErr('Необходима авторизация *'); // генерим ошибку в синхронном коде,
-      // так допустимо.
-      // Либо через next(new Err..()...) чтобы затем передать в общий обработчик ошибок
-      // res.status(401).send({ message: 'Требуется авторизация' });
-    }
-
-    const jwt = authorization.replace('Bearer ', '');
+    // проверить что jwt валидный, с помощью библиотеки jsonwebtoken
     payload = jsonwebtoken.verify(jwt, 'some-secret-key');
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    throw new AuthoErr('Необходима авторизация **');
     // res.status(401).send({ message: 'Необходима авторизация' });
   }
-
   // const { authorization } = req.headers;
   // if (!authorization || !authorization.startsWith('Bearer ')) {
   //   throw new AuthoErr('Необходима авторизация *'); // генерим ошибку в синхронном коде,
@@ -50,7 +47,7 @@ const auth = (req, res, next) => {
 
   // добавить пейлоуд токена в объект запроса юзера !!!!!!!!!!!!
   req.user = payload; // 3.если все хорошо -> иди дальше 'go next' (пропустить запрос)
-  return next();
+  next();
 };
 // const auth = (req, res, next) => {
 // // ToDo: check token valid, and go next. If (valid) {go next}, else error
